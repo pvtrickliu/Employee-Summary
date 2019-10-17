4d61b9fd7236e6da113a72ecf3f0373da7c43e64
Patrick Liu <patrickyenliu@gmail.com>
2019-10-16 22:30:02 -0700

    added function to generate html to app.js using template literals

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var inquirer = require("inquirer");
 var Employee = require("./lib/employee");
+const util = require("util");
 var team = [];
 
+//const writeFileAsync = util.promisify(fs.writeFile);
 
 function main()
 {
     console.log("start building your team.")
-    inquirer.prompt([
+    return inquirer.prompt([
         {
             type:"list",
             name:"employeeType",
@@ -29,14 +31,57 @@ function main()
             message: "What is the employee's email?"
         }
     ])
-    .then(function(results){
     
-        const {employeeName, employeeID, employeeEmail} = results;
-        const employeeToAdd = new Employee(employeeName, employeeID, employeeEmail);
-        team.push(employeeToAdd);
-        console.log(team);
-        
-    })
 }
 
-main();
+function generateHTML(answers) {
+    console.log(answers);
+    return `
+   <!DOCTYPE html>
+   <html lang="en">
+   <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <title>Document</title>
+   </head>
+   <body>
+    <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4">Name: ${answers.employeeName}</h1>
+        <ul>
+            <li class="list-group-item">Employee ID: ${answers.employeeID}</li>
+            <li class="list-group-item">Employee Email: ${answers.employeeEmail}</li>
+        </ul>
+    </div>
+    </div>
+   </body>
+   </html>`;
+    }
+
+   main()
+   .then(function(results){
+    
+    const {employeeName, employeeID, employeeEmail} = results;
+    const employeeToAdd = new Employee(employeeName, employeeID, employeeEmail);
+    team.push(employeeToAdd);
+    console.log(team);
+    // prompt user again
+    // once they say no more, then pass your array of team members to a function that generates HTML
+
+})
+
+    .then(function(answers) {
+    console.log(answers)
+      const html = generateHTML(answers);
+      return writeFileAsync("team.html", html);
+    })
+
+    .then(function() {
+      console.log("Successfully wrote to team.html");
+    })
+
+    .catch(function(err) {
+      console.log(err);
+    });
+
